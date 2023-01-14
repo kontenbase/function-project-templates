e658ae727f7575443890517bc9916eaed1cae400
drScripts <nathanael.vd@gmail.com>
2023-01-14 13:44:52 +0700

    chore: change email name

--- sendemail/index.js
+++ sendemail/index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 
 const SMTP = async (req, res) => {
     try {
-        const { from = "automation@microgen.id", to, subject = "Lorem Ipsum", content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." } = req.body;
+        const { from = "automation@kontenbase.com", to, subject = "Lorem Ipsum", content = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." } = req.body;
         const mailOptions = {
             from,
             to,
