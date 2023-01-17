cd9713ea29e1e4cb84d6b46194a22c07c00788c1
drScripts <nathanael.vd@gmail.com>
2023-01-17 23:47:45 +0700

    chore: add hardcoded credential

--- sendemail/index.js
+++ sendemail/index.js
@@ -19,11 +19,11 @@ const SMTP = async (req, res) => {
         };
 
         const transporter = nodemailer.createTransport({
-            host: process.env.SMTP_HOST,
-            port: process.env.SMTP_PORT,
+            host: "smtp-relay.sendinblue.com",
+            port: 587,
             auth: {
-                user: process.env.SMTP_EMAIL_ADDRESS,
-                pass: process.env.SMTP_EMAIL_PASSWORD,
+                user: "mejik.dev@gmail.com",
+                pass: "LQ8cHpkEqPXaAmR1",
             },
         });
 
